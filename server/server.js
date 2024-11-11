de7ec330ca17ce56773ffce892c7dfef4ad4b148
Ubuntu <ubuntu@ip-172-31-5-145.us-west-1.compute.internal>
2024-11-11 00:05:36 +0000

    initial commit for portfolio site from EC2

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// serve static files from react app
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+// exmaple API route
+app.get('/api/message', (req, res) => {
+	res.json({ message: 'Hello from the back-end' });
+});
+
+// catch-all route to serve react app
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+});
+
+app.listen(PORT, () => {
+	console.log('Server is running on port ${PORT}');
+});
