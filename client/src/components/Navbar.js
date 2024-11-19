2394ede952b8a3673e6f163e6f4eec4ee7a244dc
alternateseal <altcl.99@gmail.com>
2024-11-18 23:09:46 -0800

    added navbar files

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React, {useState} from 'react'
+
+function Navbar() {
+  return (
+    <>
+        <nav className="navbar">
+            .navbar-container
+        
+        </nav>
+    </>
+  )
+}
+
+export default Navbar
