eaf766e3b8ae785441b6f95c21a5ba4f589a17f6
alternateseal <altcl.99@gmail.com>
2024-11-21 00:23:43 -0800

    fixed app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Navbar from './components/Navbar';
+import Navbar from './component/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './component/pages/Home';
