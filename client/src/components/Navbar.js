373b02c3ee3c5a4f9ae7c20b67eea34c42f82e9b
alternateseal <altcl.99@gmail.com>
2024-11-21 02:04:48 -0800

    change stuff

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom';
+import './Navbar.css';
 import { Button } from './Button';
-import '.NavBar.css';
 
 function Navbar() {
   const [click, setClick] = useState(false);
@@ -19,14 +19,19 @@ function Navbar() {
     }
   }; 
 
+useEffect(() => {
+  showButton();
+}, []);
+
 window.addEventListener('resize', showButton);
 
   return (
     <>
         <nav className='navbar'>
             <div className='navbar-container'>
-              <Link to='/' className='navbar-logo'>
-              krbrk <i className='fab fa-typo3' />
+              <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
+              krbrk 
+              <i className='fab fa-typo3' />
               </Link>
               <div className='menu-icon' onClick={handleClick}>
                 <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
@@ -52,7 +57,7 @@ window.addEventListener('resize', showButton);
             </div>
         </nav>
     </>
-  );
+  )
 }
 
-export default Navbar;
+export default Navbar
