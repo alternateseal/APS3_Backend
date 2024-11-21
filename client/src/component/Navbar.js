066ab244357f2cde9629216524d46977b651c0dc
alternateseal <altcl.99@gmail.com>
2024-11-21 00:31:25 -0800

    button updated

--- client/src/component/Navbar.js
+++ client/src/component/Navbar.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
+import { Button } from './Button';
 
 function Navbar() {
   const [click, setClick] = useState(false);
