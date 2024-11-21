1114ab9ebd57c3e6ac9eafa35599b54e0b61a851
alternateseal <altcl.99@gmail.com>
2024-11-20 23:41:40 -0800

    pushy

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Navbar from './components/Navbar';
-import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Home from '.component/pages/Home';
 
@@ -8,14 +8,12 @@ import Home from '.component/pages/Home';
 function App() {
   return (
     <>
-    <Router>
-      <Navbar />
-      <Switch>
-        <Route path='/' exact component=
-        {Home} />
-      </Switch>
-    </Router>
-      <Navbar />
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component={Home} />
+        </Switch>
+      </Router>
     </>
   );
 }
