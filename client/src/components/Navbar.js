1e85a9ae63e3a1a1ccda2640af83d1394e6fec75
alternateseal <altcl.99@gmail.com>
2024-11-20 23:50:48 -0800

    navbar changes

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 
 function Navbar() {
@@ -50,7 +50,7 @@ window.addEventListener('resize', showButton);
             </div>
         </nav>
     </>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
