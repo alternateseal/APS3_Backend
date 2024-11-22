7a276f24661c1e74e5dce304834b5a59e86907a1
alternateseal <altcl.99@gmail.com>
2024-11-22 15:27:12 -0800

    changed some button positions

--- client/src/component/Button.js
+++ client/src/component/Button.js
@@ -22,10 +22,10 @@ export const Button = ({
         return (
             <Link to='/contact' className='btn-mobile'>
                 <button
-                    className={'btn ${checkButtonStyle} ${checkButtonSize}'}
+                    className={`btn ${checkButtonStyle} ${checkButtonSize}`}
                     onClick={onClick}
                     type={type}
-                    >
+                >
                     {children}
                 </button>
             </Link>
