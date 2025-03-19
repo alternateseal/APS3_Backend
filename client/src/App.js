a034350e12f75969dbe67def93d0312360808abc
alternateseal <altcl.99@gmail.com>
2025-03-18 17:52:10 -0700

    changed some divs

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,15 @@
 import React from "react";
 import Nav from './component/Nav';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <>
-      <div ClassName="header-wrapper">
         <Nav />
-      </div>
     </>
   );
 }
 
-export default App;
+export default App
 
 /* CHANGE PLEASE */
