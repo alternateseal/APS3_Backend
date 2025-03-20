f4f111ee101e186c1797436d424c322afd065724
alternateseal <altcl.99@gmail.com>
2025-03-19 23:28:37 -0700

    remove strategic line for better experience

--- client/src/index/index.js
+++ client/src/index/index.js
@@ -20,7 +20,7 @@ function Index () {
                     <div className={indexCSS.headerContent}>
                         <h5>Hello, I am</h5>
                         <h1>Kyle</h1>
-                        <p>A Strategic&nbsp;
+                        <p>A&nbsp;
                             <span>
                                 <ReactTyped
                                     strings={['Web Designer' , 'App Designer' , 'UI / UX Designer' , 'Full-stack Developer' , 'Database Architect' , 'API Developer' , 'Backend Engineer' , 'Reverse Engineer']}
